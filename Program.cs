4ba2da076b653f30bbd1a865c2b8eecede6875e8
pranali2808 <ranepranali1997@gmail.com>
2022-04-14 14:47:39 +0530

    ability to create BST by adding 56

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Binary Search Tree *****");
+            BinaryTree<int> binarySearch = new BinaryTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            Console.ReadLine();
+        }
+    }
+       
+}
