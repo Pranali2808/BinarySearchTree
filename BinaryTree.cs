6a90bbada7f81e013a64b3504ffe0a9491082e75
pranali2808 <ranepranali1997@gmail.com>
2022-04-21 14:44:09 +0530

    find the size of binary tree

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -28,6 +28,7 @@ namespace BinarySearchTree
                     this.LeftTree = new BinaryTree<T>(item);
                 else
                     this.LeftTree.Insert(item);
+                     this.leftCount++;
             }
             else
             {
@@ -35,19 +36,20 @@ namespace BinarySearchTree
                     this.RightTree = new BinaryTree<T>(item);
                 else
                     this.RightTree.Insert(item);
+                    this.rightCount++;
             }
         }
         public void Display()
         {
             if (this.LeftTree != null)
             {
-                this.leftCount++;
+               
                 this.LeftTree.Display();
             }
             Console.WriteLine(this.NodeData.ToString());
             if (this.RightTree != null)
             {
-                this.rightCount++;
+               
                 this.RightTree.Display();
             }
         }
